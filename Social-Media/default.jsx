377bee78ffe25f9af23155fd7f90abe088665ebd
Pablo Cubillos <pcubillos@elespectador.com>
2020-01-27 14:30:22 -0500

    Adding social media component

---
+++ Social-Media/default.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from 'react';
+
+class SocialMedia extends Component {
+
+    render(){
+        return (<div>Lorem ipsum</div>);
+    }
+}
+
+export default SocialMedia;
